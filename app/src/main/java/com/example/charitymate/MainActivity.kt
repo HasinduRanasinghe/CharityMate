a8bd179258ad163d5f1be13d48758f1afc4072a3
HasinduRanasinghe <hasinduranasingheb2098@gmail.com>
2023-05-01 12:37:12 +0530

    CharityMate welcome, user welcome and register pages

--- app/src/main/java/com/example/charitymate/MainActivity.kt
+++ app/src/main/java/com/example/charitymate/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.charitymate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.welcome_to_charity_mate)
     }
+
+    fun displayUserWelcome(view: View){
+        setContentView(R.layout.user_welcome)
+    }
+
+    fun displayWelcomePage(view: View){
+        setContentView(R.layout.welcome_to_charity_mate)
+    }
+
+    fun displayRegisterPage(view: View){
+        setContentView(R.layout.register_page)
+    }
+
 }
