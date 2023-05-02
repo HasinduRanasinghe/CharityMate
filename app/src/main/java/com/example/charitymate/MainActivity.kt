14c3248ddcc7d1a77fc011b955052ac97ef05b38
HasinduRanasinghe <hasinduranasingheb2098@gmail.com>
2023-05-02 07:10:21 +0530

    Connected to firebase

--- app/src/main/java/com/example/charitymate/MainActivity.kt
+++ app/src/main/java/com/example/charitymate/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.charitymate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    val dataBase = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
